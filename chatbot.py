8064a4304d04d113dbdd92b2bc62e27b428f1b88
restuprajna <prajna.restu.rp@gmail.com>
2024-08-17 14:25:00 +0800

    implement langchain backend api to generate response

--- chatbot.py
+++ chatbot.py
@@ -1,12 +1,13 @@
+import os
 import streamlit as st
 from openai import OpenAI
+from dotenv import load_dotenv
+import json
+import requests
 
-with st.sidebar:
-    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+load_dotenv()
 
+gemini_api_key: str=os.environ["GEMINI_API_KEY"]
 st.title("💬 Chatbot")
 
 if "messages" not in st.session_state:
@@ -15,15 +16,40 @@ if "messages" not in st.session_state:
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
-if prompt := st.chat_input():
-    if not openai_api_key:
-        st.info("Please add your OpenAI API key to continue.")
-        st.stop()
 
-    client = OpenAI(api_key=openai_api_key)
+
+
+def generate_response(question: str):
+    url="http://127.0.0.1:8000/vertex-ai/{instance_id}/invoke"
+
+    headers_template = {
+        'accept': 'application/json',
+        'Content-Type': 'application/json',
+        "Authorization": "Bearer test",
+    }
+
+    data = {
+            "input": question,
+            "config": {},
+            "kwargs": {}
+    }
+
+    req_generate = requests.post(url, headers=headers_template, json=data)
+    res_data = json.loads(req_generate.text)
+    return res_data
+
+if prompt := st.chat_input():
+    # if not openai_api_key:
+    #     st.info("Please add your OpenAI API key to continue.")
+    #     st.stop()
+
+    # client = OpenAI(api_key=gemini_api_key)
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-    response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
-    msg = response.choices[0].message.content
-    st.session_state.messages.append({"role": "assistant", "content": msg})
-    st.chat_message("assistant").write(msg)
+    # response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    response = generate_response(question=prompt)
+    print(prompt)
+    print(response)
+    # msg = response.choices[0].message.content
+    st.session_state.messages.append({"role": "assistant", "content": response})
+    st.chat_message("assistant").write(response)
